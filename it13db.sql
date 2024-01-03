01587ef8f77bb426ef6be5024ad2a69f15338403
H-neun <142612349+H-neun@users.noreply.github.com>
2024-01-03 16:22:21 +0800

    Add files via upload

---
+++ it13db.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE
+    /*[DEFINER = root@localhost PROCEDURE procadduserinfo
+    PROCEDURE `it13`.`procadduserinfo`()
+    /*LANGUAGE SQL
+    | [NOT] DETERMINISTIC
+    | { CONTAINS SQL | NO SQL | READS SQL DATA | MODIFIES SQL DATA }
+    | SQL SECURITY { DEFINER | INVOKER }
+    | COMMENT 'string'*/
+	BEGIN
+
+	END$$
+
+DELIMITER ;
